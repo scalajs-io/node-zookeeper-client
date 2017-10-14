eba8a7cf2127be2d5bc389e0ee3c75a42e634ded
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2017-10-14 12:06:32 -0700

    Release v0.4.2

--- src/main/scala/io/scalajs/npm/nzc/ClientOptions.scala
+++ src/main/scala/io/scalajs/npm/nzc/ClientOptions.scala
@@ -1,7 +1,7 @@
 package io.scalajs.npm.nzc
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.ScalaJSDefined
+
 
 /**
   * Zookeeper Client Options
@@ -10,7 +10,7 @@ import scala.scalajs.js.annotation.ScalaJSDefined
   * @param retries        The number of retry attempts for connection loss exception.
   * @author lawrence.daniels@gmail.com
   */
-@ScalaJSDefined
+
 class ClientOptions(var sessionTimeout: js.UndefOr[Int] = js.undefined,
                     var spinDelay: js.UndefOr[Int] = js.undefined,
                     var retries: js.UndefOr[Int] = js.undefined)
